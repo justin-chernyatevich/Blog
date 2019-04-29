8411f746e3f447e8bd0168bf1b618bfeb27a087a
Justin <justin.chernyatevich@gmail.com>
2019-04-29 11:35:14 +0300

    Modified file(s)

--- blog.py
+++ blog.py
@@ -1,68 +1,77 @@
 import sqlite3
-from bottle import run, route, template,\
-                   request, redirect, abort,\
-                   static_file    
+from bottle import run, route, template, \
+    request, abort, \
+    static_file
 
 database = "Sqlite/pages.db"
+con = sqlite3.connect(database)
+cur = con.cursor()
 
-@route('/static/<filepath:path>')
-def static_files(filepath):
-    return static_file(filepath, root="")
+
+def init_db():
+    try:
+        cur.execute('''CREATE TABLE `entries` (
+                     Id	INTEGER PRIMARY KEY AUTOINCREMENT,
+    	            text	TEXT NOT NULL,
+    	            title	TEXT NOT NULL
+                        );''')
+    except sqlite3.OperationalError:
+        pass
+
+
+@route("/static/<file_path>")
+def static_files(file_path):
+    return static_file(file_path, root="static/")
+
 
 @route("/add", method="GET")
 def write():
-    return template("index.html", mess="")
+    return template("add_blog.html", mess="")
+
 
 @route('/add', method='POST')
-def writed():
+def edited():
     global database
     con = sqlite3.connect(database)
     cur = con.cursor()
     data_form = request.forms.decode("utf-8")
     data_form_text = data_form["text"]
     title = data_form["title"]
-    try:
+    if data_form_text != "" and title != "":
         cur.execute('INSERT INTO entries (text, title) VALUES(?, ?)',
-                    (data_form_text, title))
+                        (data_form_text, title))
         con.commit()
-    except sqlite3.OperationalError:
-        cur.execute('''CREATE TABLE `entries` (
-         Id	INTEGER PRIMARY KEY AUTOINCREMENT,
-	 text	TEXT NOT NULL,
-	 title	TEXT NOT NULL
-        );''')
-        cur.execute('INSERT INTO entries (text, title) VALUES(?, ?)',
-                    (data_form_text, title))
-        con.commit()
-    return template("index.html", mess=\
-                    "New entry was successfully posted")
+        return template("add_blog.html", mess="New entry was successfully posted")
+    else:
+        return template("add_blog.html", mess="Write error")
+
+
+@route('/')
+def dis_blog():
+    cur.execute('SELECT * FROM entries')
+    rows = cur.fetchall()
+    return template("index.html", mess=rows)
+
 
 @route('/blog/<id_blog>')
 def display_entries(id_blog):
-    global database
-    con = sqlite3.connect(database)
-    cur = con.cursor()
-    mlist = []
-    try:
-        cur.execute('SELECT * FROM entries')
-    except sqlite3.OperationalError:
-        cur.execute('''CREATE TABLE `entries` (
-         Id	INTEGER PRIMARY KEY AUTOINCREMENT,
-	 text	TEXT NOT NULL,
-	 title	TEXT NOT NULL
-        );''')
-        cur.execute('SELECT * FROM entries')
+    my_list = []
+    cur.execute('SELECT * FROM entries')
     for i in cur:
-        mlist.append(i[0])
+        my_list.append(i[0])
     try:
-        if int(id_blog) in mlist:
+        if int(id_blog) in my_list:
             cur.execute('SELECT * FROM entries WHERE Id IN (%s);' % id_blog)
             for i in cur:
-                #yield "<h1>%s</h1>" % i[2]
-                #yield "<pre>%s\n</pre>" % i[1]
+                # yield "<h1>%s</h1>" % i[2]
+                # yield "<pre>%s\n</pre>" % i[1]
                 return template("blog.html", title=i[2], text=i[1])
     except ValueError:
         abort(404, "Not found: '/blog/%s'" % str(id_blog))
     else:
         abort(404, "Not found: '/blog/%s'" % str(id_blog))
-run(host="0.0.0.0", port="9000")
+
+
+if __name__ == "__main__":
+    init_db()
+    run(host="192.168.88.225", port="9100")
